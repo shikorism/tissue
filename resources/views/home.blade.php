f51aaea94cfb2dcdadfb12ba3667fad1be1f8aab
shibafu <shibafu528@gmail.com>
2018-06-07 02:23:09 +0900

    お惣菜コーナーにも「同じオカズでチェックイン」ボタンを表示する

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,6 +37,9 @@
                                         <a href="{{ route('user.profile', ['id' => $ejaculation->user->name]) }}" class="text-dark"><img src="{{ $ejaculation->user->getProfileImageUrl(30) }}" width="30" height="30" class="rounded d-inline-block align-bottom"> &commat;{{ $ejaculation->user->name }}</a>
                                         <a href="{{ route('checkin.show', ['id' => $ejaculation->id]) }}" class="text-muted"><small>{{ $ejaculation->ejaculated_date->format('Y/m/d H:i') }}</small></a>
                                     </h5>
+                                    <div>
+                                        <a class="text-secondary timeline-action-item" href="{{ route('checkin', ['link' => $ejaculation->link, 'tags' => $ejaculation->textTags()]) }}"><span class="oi oi-reload" data-toggle="tooltip" data-placement="bottom" title="同じオカズでチェックイン"></span></a>
+                                    </div>
                                 </div>
                                 <!-- tags -->
                                 @if ($ejaculation->tags->isNotEmpty())
