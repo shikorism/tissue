9646f90ce34a7eba625221deb72c72413c41a404
shibafu <shibafu528@gmail.com>
2018-06-13 01:00:24 +0900

    通販サイトのメタデータ対応

---
+++ app/MetadataResolver/ToranoanaResolver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\MetadataResolver;
+
+use GuzzleHttp\Cookie\CookieJar;
+
+class ToranoanaResolver implements Resolver
+{
+    public function resolve(string $url): Metadata
+    {
+        $cookieJar = CookieJar::fromArray(['adflg' => '0'], 'ec.toranoana.jp');
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->get($url, ['cookies' => $cookieJar]);
+        if ($res->getStatusCode() === 200) {
+            $ogpResolver = new OGPResolver();
+            return $ogpResolver->parse($res->getBody());
+        } else {
+            throw new \RuntimeException("{$res->getStatusCode()}: $url");
+        }
+    }
+}
