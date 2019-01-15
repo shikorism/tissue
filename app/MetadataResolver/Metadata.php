5f01cc3430ac9258187ff8f6f43e50719310581c
shibafu <shibafu528@gmail.com>
2019-01-15 23:56:04 +0900

    メタデータエンティティにexpires_atプロパティを追加

--- app/MetadataResolver/Metadata.php
+++ app/MetadataResolver/Metadata.php
@@ -2,9 +2,13 @@
 
 namespace App\MetadataResolver;
 
+use Carbon\Carbon;
+
 class Metadata
 {
     public $title = '';
     public $description = '';
     public $image = '';
+    /** @var Carbon|null */
+    public $expires_at = null;
 }
