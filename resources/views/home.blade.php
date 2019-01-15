f367ec212f9e6d54a7cdc8fd9eba257b3d4e567e
eai04191 <eai@mizle.net>
2019-01-16 07:25:06 +0900

    リンクアイコンの親要素をflexboxにしてリンクがアイコンから落ちないようにする

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,8 +61,8 @@
                                                 </div>
                                             </a>
                                         </div>
-                                        <p class="mb-2 col-12 px-0">
-                                            <span class="oi oi-link-intact mr-1"></span><a href="{{ $ejaculation->link }}" target="_blank" rel="noopener">{{ $ejaculation->link }}</a>
+                                        <p class="d-flex align-items-baseline mb-2 col-12 px-0">
+                                            <span class="oi oi-link-intact mr-1"></span><a class="overflow-hidden" href="{{ $ejaculation->link }}" target="_blank" rel="noopener">{{ $ejaculation->link }}</a>
                                         </p>
                                     </div>
                                 @endif
