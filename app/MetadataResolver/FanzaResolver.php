0dcd105520d07bff521b1927f1fb4f28878a8b15
eai04191 <eai@mizle.net>
2019-01-17 00:00:09 +0900

    FanzaResolver追加
    FanzaのOGP画像は `ps.jpg` か `pr.jpg` で終わっている。 `pl.jpg` で高解像度なものが取得できる。

---
+++ app/MetadataResolver/FanzaResolver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\MetadataResolver;
+
+class FanzaResolver implements Resolver
+{
+    public function resolve(string $url): Metadata
+    {
+        $client = new \GuzzleHttp\Client();
+        $res = $client->get($url);
+        if ($res->getStatusCode() === 200) {
+            $ogpResolver = new OGPResolver();
+            $metadata = $ogpResolver->parse($res->getBody());
+            $metadata->image =  preg_replace("~(pr|ps).jpg$~", "pl.jpg", $metadata->image);
+
+            return $metadata;
+        } else {
+            throw new \RuntimeException("{$res->getStatusCode()}: $url");
+        }
+    }
+}
