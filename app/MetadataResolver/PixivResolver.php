c6b747a04e04d489fb5ff12e0db9559b9939796f
eai04191 <eai@mizle.net>
2019-02-12 07:46:01 +0900

    pixivのOGP画像にすべてプロキシURLを使用する

--- app/MetadataResolver/PixivResolver.php
+++ app/MetadataResolver/PixivResolver.php
@@ -41,52 +41,24 @@ class PixivResolver implements Resolver
 
             // 未ログインでは漫画ページを開けないため、URL を作品ページに変換する
             $url = preg_replace('~mode=manga(_big)?~', 'mode=medium', $url);
+        }
 
-            $client = new \GuzzleHttp\Client();
-            $res = $client->get($url);
-            if ($res->getStatusCode() === 200) {
-                $ogpResolver = new OGPResolver();
-                $metadata = $ogpResolver->parse($res->getBody());
+        $client = new \GuzzleHttp\Client();
+        $res = $client->get($url);
+        if ($res->getStatusCode() === 200) {
+            $ogpResolver = new OGPResolver();
+            $metadata = $ogpResolver->parse($res->getBody());
 
-                preg_match("~https://i\.pximg\.net/c/128x128/img-master/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/{$illustId}_p0_square1200\.jpg~", $res->getBody(), $match);
-                $illustThumbnailUrl = $match[0];
+            preg_match("~https://i\.pximg\.net/c/128x128/img-master/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/{$illustId}(_p0)?_square1200\.jpg~", $res->getBody(), $match);
+            $illustThumbnailUrl = $match[0];
 
-                $illustUrl = $this->thumbnailToMasterUrl($illustThumbnailUrl);
+            $illustUrl = $this->thumbnailToMasterUrl($illustThumbnailUrl);
 
-                // 指定ページに変換
-                $illustUrl = str_replace('p0_master', "p{$page}_master", $illustUrl);
+            $metadata->image =  $this->proxize($illustUrl);
 
-                $metadata->image =  $this->proxize($illustUrl);
-
-                return $metadata;
-            } else {
-                throw new \RuntimeException("{$res->getStatusCode()}: $url");
-            }
+            return $metadata;
         } else {
-            $client = new \GuzzleHttp\Client();
-            $res = $client->get($url);
-            if ($res->getStatusCode() === 200) {
-                $ogpResolver = new OGPResolver();
-                $metadata = $ogpResolver->parse($res->getBody());
-
-                // OGP がデフォルト画像であるようならなんとかして画像を取得する
-                if (strpos($metadata->image, 'pixiv_logo.gif') || strpos($metadata->image, 'pictures.jpg')) {
-
-                    // 作品ページの場合のみ対応
-                    if ($params['mode'] === 'medium') {
-                        preg_match("~https://i\.pximg\.net/c/128x128/img-master/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/{$illustId}(_p0)?_square1200\.jpg~", $res->getBody(), $match);
-                        $illustThumbnailUrl = $match[0];
-
-                        $illustUrl = $this->thumbnailToMasterUrl($illustThumbnailUrl);
-
-                        $metadata->image =  $this->proxize($illustUrl);
-                    }
-                }
-
-                return $metadata;
-            } else {
-                throw new \RuntimeException("{$res->getStatusCode()}: $url");
-            }
+            throw new \RuntimeException("{$res->getStatusCode()}: $url");
         }
     }
 }
