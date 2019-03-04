6a1848e3115773399eed20f77906c3e7517ac93e
shibafu <shibafu528@gmail.com>
2019-03-05 00:06:04 +0900

    既存のファイル化されているフロントコードを全てassetsに移動し、mixビルドの準備

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .autoload({
+        'jquery': ['$', 'jQuery', 'window.jQuery']
+    });
