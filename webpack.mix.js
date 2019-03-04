d4d98db686b5278e5004f33517326cfe9b99c779
shibafu <shibafu528@gmail.com>
2019-03-05 00:25:13 +0900

    ホーム画面のインラインスクリプトをhome.jsに移動

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/home.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .autoload({
         'jquery': ['$', 'jQuery', 'window.jQuery']
