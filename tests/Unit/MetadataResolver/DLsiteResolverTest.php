14b57bf9f505f0e3ea20dc140f559975e4f86240
shibafu <shibafu528@gmail.com>
2019-03-17 20:07:44 +0900

    fix DLsiteResolver test
    refs #132

--- tests/Unit/MetadataResolver/DLsiteResolverTest.php
+++ tests/Unit/MetadataResolver/DLsiteResolverTest.php
@@ -55,8 +55,8 @@ class DLsiteResolverTest extends TestCase
         $this->createResolver(DLsiteResolver::class, $responseText);
 
         $metadata = $this->resolver->resolve('https://dlsite.jp/mawtw/RJ171695.html');
-        $this->assertEquals('【骨伝導風】道草屋 たびらこ-一緒にはみがき【耳かき&はみがき】 [桃色CODE] | DLsite', $metadata->title);
-        $this->assertStringStartsWith('少しお母さんっぽい店員さんに、歯磨きからおやすみまでお世話されます。はみがきで興奮しちゃった旦那様のも、しっかりお世話してくれます。歯磨き音は特殊なマイクを使用、骨伝導風ハイレゾバイノーラル音声です。', $metadata->description);
+        $this->assertEquals('【骨伝導風】道草屋 たびらこ-一緒にはみがき【耳かき&はみがき】', $metadata->title);
+        $this->assertStringEndsWith('少しお母さんっぽい店員さんに、歯磨きからおやすみまでお世話されます。はみがきで興奮しちゃった旦那様のも、しっかりお世話してくれます。歯磨き音は特殊なマイクを使用、骨伝導風ハイレゾバイノーラル音声です。', $metadata->description);
         $this->assertEquals('https://img.dlsite.jp/modpub/images2/work/doujin/RJ172000/RJ171695_img_main.jpg', $metadata->image);
         if ($this->shouldUseMock()) {
             $this->assertSame('https://dlsite.jp/mawtw/RJ171695.html', (string) $this->handler->getLastRequest()->getUri());
