a7859fdda6e0b4627ded5a27ee85ae2dc61a069a
shibafu <shibafu528@gmail.com>
2019-03-19 00:56:31 +0900

    set -eしてるんだから落ちるだろうが

--- dist/bin/tissue-entrypoint.sh
+++ dist/bin/tissue-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$APP_DEBUG" == "true" ]]; then
   export PHP_INI_SCAN_DIR=":/usr/local/etc/php/php.d"
 
-  php -r "if (gethostbyname('host.docker.internal') === 'host.docker.internal') exit(1);" &> /dev/null
+  php -r "if (gethostbyname('host.docker.internal') === 'host.docker.internal') exit(1);" &> /dev/null && :
   if [[ $? -eq 0 ]]; then
     # Docker for Windows/Mac
     export PHP_XDEBUG_REMOTE_HOST='host.docker.internal'
