80fe53cf20f67abdfc8b7222f6d7047be3820820
shibafu <shibafu528@gmail.com>
2019-03-18 23:22:28 +0900

    Linux+Dockerな開発環境でxdebugが使えるように、コールバック先IPアドレスの設定を調整

--- dist/bin/tissue-entrypoint.sh
+++ dist/bin/tissue-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 if [[ "$APP_DEBUG" == "true" ]]; then
   export PHP_INI_SCAN_DIR=":/usr/local/etc/php/php.d"
+  export PHP_XDEBUG_REMOTE_HOST=$(cat /etc/hosts | awk 'END{print $1}' | sed -r -e 's/[0-9]+$/1/g')
 fi
 
 exec docker-php-entrypoint "$@"
