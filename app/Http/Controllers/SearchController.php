9cec184d210e1cc9a24dc78fb5af5f54ade7177a
shibafu <shibafu528@gmail.com>
2019-04-07 00:10:01 +0900

    チェックイン非公開ユーザーのチェックインが検索結果として表示される問題の修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Ejaculation;
 use App\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
@@ -17,6 +18,12 @@ class SearchController extends Controller
         $results = Ejaculation::query()
             ->whereHas('tags', function ($query) use ($inputs) {
                 $query->where('name', 'like', "%{$inputs['q']}%");
+            })
+            ->whereHas('user', function ($query) {
+                $query->where('is_protected', false);
+                if (Auth::check()) {
+                    $query->orWhere('id', Auth::id());
+                }
             })
             ->where('is_private', false)
             ->orderBy('ejaculated_date', 'desc')
