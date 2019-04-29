e4c942263adcc6340567640823ab95c2e951561d
shibafu <shibafu528@gmail.com>
2019-04-29 12:26:29 +0900

    MetadataResolverからタグ情報を保存できるようにした

--- app/MetadataResolver/Metadata.php
+++ app/MetadataResolver/Metadata.php
@@ -6,9 +6,21 @@ use Carbon\Carbon;
 
 class Metadata
 {
+    /** @var string タイトル */
     public $title = '';
+
+    /** @var string 概要 */
     public $description = '';
+
+    /** @var string サムネイルのURL */
     public $image = '';
-    /** @var Carbon|null */
+
+    /** @var Carbon|null メタデータの有効期限 */
     public $expires_at = null;
+
+    /**
+     * @var string[] タグ
+     * チェックインタグと同様に保存されるため、スペースや改行文字を含めてはいけません。
+     */
+    public $tags = [];
 }
