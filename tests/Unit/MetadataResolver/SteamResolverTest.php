16e5341de1b1f50683480b188883cca4170b0d25
eai04191 <eai@mizle.net>
2019-06-17 04:29:54 +0900

    assertStringStartsWithに変更
    モックを使わないテストがコケることがあるので

--- tests/Unit/MetadataResolver/SteamResolverTest.php
+++ tests/Unit/MetadataResolver/SteamResolverTest.php
@@ -27,7 +27,7 @@ class SteamResolverTest extends TestCase
         $metadata = $this->resolver->resolve('https://store.steampowered.com/app/333600');
         $this->assertEquals('NEKOPARA Vol. 1', $metadata->title);
         $this->assertEquals('水無月嘉祥(みなづき かしょう)は伝統ある老舗和菓子屋である実家を出て、 パティシエとして自身のケーキ屋『ラ・ソレイユ』を一人で開店する。 しかし実家から送った引っ越し荷物の中に、 実家で飼っていた人型ネコのショコラとバニラが紛れ込んでいた。', $metadata->description);
-        $this->assertEquals('https://steamcdn-a.akamaihd.net/steam/apps/333600/header.jpg?t=1558382831', $metadata->image);
+        $this->assertStringStartsWith('https://steamcdn-a.akamaihd.net/steam/apps/333600/header.jpg?t=', $metadata->image);
     }
 
     public function testR18()
@@ -39,7 +39,7 @@ class SteamResolverTest extends TestCase
         $metadata = $this->resolver->resolve('https://store.steampowered.com/app/1077580');
         $this->assertEquals('Broke Girl  | 負債千金', $metadata->title);
         $this->assertEquals('苦労知らずに育ったお嬢様は一夜にして1000万の借金を背負うことになった。借金を返済するために働かなければならない。しかし世間には悪意が満ちており、男達はお金で彼女を誘うか凌辱することしか考えていない。', $metadata->description);
-        $this->assertEquals('https://steamcdn-a.akamaihd.net/steam/apps/1077580/header.jpg?t=1559506319', $metadata->image);
+        $this->assertStringStartsWith('https://steamcdn-a.akamaihd.net/steam/apps/1077580/header.jpg?t=', $metadata->image);
     }
 
     public function testNotFound()
