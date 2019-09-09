59aec2c038f5079912e353d579263eb61ec4c335
shibafu <shibafu528@gmail.com>
2019-09-10 01:01:47 +0900

    不要な成否判定をやめる

--- app/MetadataResolver/NicoSeigaResolver.php
+++ app/MetadataResolver/NicoSeigaResolver.php
@@ -25,22 +25,18 @@ class NicoSeigaResolver implements Resolver
     public function resolve(string $url): Metadata
     {
         $res = $this->client->get($url);
-        if ($res->getStatusCode() === 200) {
-            $html = (string)$res->getBody();
-            $metadata = $this->ogpResolver->parse($html);
-            $crawler = new Crawler($html);
+        $html = (string)$res->getBody();
+        $metadata = $this->ogpResolver->parse($html);
+        $crawler = new Crawler($html);
 
-            // タグ
-            $excludeTags = ['R-15'];
-            $metadata->tags = array_values(array_diff($crawler->filter('.tag')->extract(['_text']), $excludeTags));
+        // タグ
+        $excludeTags = ['R-15'];
+        $metadata->tags = array_values(array_diff($crawler->filter('.tag')->extract(['_text']), $excludeTags));
 
-            // ページURLからサムネイルURLに変換
-            preg_match('~https?://(?:(?:sp\\.)?seiga\\.nicovideo\\.jp/seiga(?:/#!)?|nico\\.ms)/im(\\d+)~', $url, $matches);
-            $metadata->image = "https://lohas.nicoseiga.jp/thumb/${matches[1]}l?";
+        // ページURLからサムネイルURLに変換
+        preg_match('~https?://(?:(?:sp\\.)?seiga\\.nicovideo\\.jp/seiga(?:/#!)?|nico\\.ms)/im(\\d+)~', $url, $matches);
+        $metadata->image = "https://lohas.nicoseiga.jp/thumb/${matches[1]}l?";
 
-            return $metadata;
-        } else {
-            throw new \RuntimeException("{$res->getStatusCode()}: $url");
-        }
+        return $metadata;
     }
 }
