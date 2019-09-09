eecace33bd66bf5e84901867006bfc0679a35b39
eai04191 <eai@mizle.net>
2019-09-09 10:58:20 +0900

    2桁以上のcdnのURLに対応できるように正規表現を修正

--- tests/Unit/MetadataResolver/XtubeResolverTest.php
+++ tests/Unit/MetadataResolver/XtubeResolverTest.php
@@ -27,7 +27,7 @@ class XtubeResolverTest extends TestCase
         $metadata = $this->resolver->resolve('https://www.xtube.com/video-watch/homegrown-big-tits-18634762');
         $this->assertEquals('Homegrown Big Tits', $metadata->title);
         $this->assertEquals('Dedicated to the fans of the beautiful amateur women with big natural tits.  All user submitted - you can see big boob amateur hotties fucking and sucking as their tits bounce and sway.', $metadata->description);
-        $this->assertRegExp('~https://cdn\d-s-hw-e5\.xtube\.com/m=eSK08f/videos/201302/07/RF4Nk-S774-/240X180/1\.jpg~', $metadata->image);
+        $this->assertRegExp('~https://cdn\d+-s-hw-e5\.xtube\.com/m=eSK08f/videos/201302/07/RF4Nk-S774-/240X180/1\.jpg~', $metadata->image);
         $this->assertEquals(['bigtits', 'homeg'], $metadata->tags);
     }
 
