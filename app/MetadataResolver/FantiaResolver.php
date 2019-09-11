a9a0f3b99ac35d02a33929658a7609635d6880a9
eai04191 <eai@mizle.net>
2019-09-12 06:56:28 +0900

    FantiaResolverを更新・テストを追加
    APIを見つけたのでAPIを使用するように
    タグを追加
    テストを追加

--- app/MetadataResolver/FantiaResolver.php
+++ app/MetadataResolver/FantiaResolver.php
@@ -3,7 +3,6 @@
 namespace App\MetadataResolver;
 
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Log;
 
 class FantiaResolver implements Resolver
 {
@@ -11,41 +10,30 @@ class FantiaResolver implements Resolver
      * @var Client
      */
     private $client;
-    /**
-     * @var OGPResolver
-     */
-    private $ogpResolver;
 
-    public function __construct(Client $client, OGPResolver $ogpResolver)
+    public function __construct(Client $client)
     {
         $this->client = $client;
-        $this->ogpResolver = $ogpResolver;
     }
 
     public function resolve(string $url): Metadata
     {
-        preg_match("~\d+~", $url, $match);
-        $postId = $match[0];
+        preg_match("~posts/(\d+)~", $url, $match);
+        $postId = $match[1];
 
-        $res = $this->client->get($url);
-        $metadata = $this->ogpResolver->parse($res->getBody());
+        $res = $this->client->get("https://fantia.jp/api/v1/posts/{$postId}");
+        $data = json_decode(str_replace('\r\n', '\n', (string) $res->getBody()), true);
+        $post = $data['post'];
 
-        $dom = new \DOMDocument();
-        @$dom->loadHTML(mb_convert_encoding($res->getBody(), 'HTML-ENTITIES', 'UTF-8'));
-        $xpath = new \DOMXPath($dom);
+        $tags = array_map(function ($tag) {
+            return $tag['name'];
+        }, $post['tags']);
 
-        $node = $xpath->query("//meta[@property='twitter:image']")->item(0);
-        $ogpUrl = $node->getAttribute('content');
-
-        // 投稿に画像がない場合（ogp.jpgでない場合）のみ大きい画像に変換する
-        if ($ogpUrl != 'http://fantia.jp/images/ogp.jpg') {
-            preg_match("~https://fantia\.s3\.amazonaws\.com/uploads/post/file/{$postId}/ogp_(.*?)\.(jpg|png)~", $ogpUrl, $match);
-            $uuid = $match[1];
-            $extension = $match[2];
-
-            // 大きい画像に変換
-            $metadata->image = "https://c.fantia.jp/uploads/post/file/{$postId}/main_{$uuid}.{$extension}";
-        }
+        $metadata = new Metadata();
+        $metadata->title = $post['title'] ?? '';
+        $metadata->description = 'サークル: ' . $post['fanclub']['fanclub_name_with_creator_name'] . PHP_EOL . $post['comment'];
+        $metadata->image = str_replace('micro', 'main', $post['thumb_micro']) ?? '';
+        $metadata->tags = array_merge($tags, [$post['fanclub']['creator_name']]);
 
         return $metadata;
     }
