a434a45e4a938f2513bce397a33672eeaf3f14c2
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2019-09-25 23:39:36 +0900

    タグ一覧の件数カウント条件を検索機能と揃える

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class TagController extends Controller
@@ -18,7 +19,14 @@ SQL
             ))
             ->join('ejaculation_tag', 'tags.id', '=', 'ejaculation_tag.tag_id')
             ->join('ejaculations', 'ejaculations.id', '=', 'ejaculation_tag.ejaculation_id')
+            ->join('users', 'users.id', '=', 'ejaculations.user_id')
             ->where('ejaculations.is_private', false)
+            ->where(function ($query) {
+                $query->where('users.is_protected', false);
+                if (Auth::check()) {
+                    $query->orWhere('users.id', Auth::id());
+                }
+            })
             ->groupBy('tags.name')
             ->orderByDesc('checkins_count')
             ->orderBy('tags.name')
