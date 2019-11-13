53560640ff48d8975184f297241d532470758650
shibafu <shibafu528@gmail.com>
2019-11-14 01:28:25 +0900

    管理ダッシュボードにどうでもいい情報を表示する

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $pgVersion = DB::select('show server_version')[0]->server_version;
+
+        return view('admin.dashboard')->with(compact('pgVersion'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,8 +3,28 @@
 @section('title', 'ダッシュボード')
 
 @section('tab-content')
-    <div class="container d-flex flex-column align-items-center">
-        <img src="{{ asset('dashboard.png') }}" class="w-50"/>
-        <p class="text-muted">TODO: 役に立つ情報を表示する</p>
+    <div class="container">
+        <div class="d-flex flex-column align-items-center">
+            <img src="{{ asset('dashboard.png') }}" class="w-50"/>
+        </div>
+        <div class="row">
+            <div class="card col-12 col-md-6 offset-md-3">
+                <div class="card-body">
+                    <h5 class="card-title">サーバー情報</h5>
+                    <div class="d-flex justify-content-between">
+                        <p class="my-0">PHP</p>
+                        <p class="my-0">{{ PHP_VERSION }}</p>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <p class="my-0">PostgreSQL</p>
+                        <p class="my-0">{{ $pgVersion }}</p>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <p class="my-0">Laravel</p>
+                        <p class="my-0">{{ app()->version() }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
