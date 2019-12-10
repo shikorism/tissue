695f45750527a0418633f7a53beab6a49f98a218
shibafu <shibafu528@gmail.com>
2019-12-11 01:32:55 +0900

    テストをしましょう

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,10 @@
+<?php
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Like::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
