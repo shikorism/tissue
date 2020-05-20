8a764c756cb0d20d2506121b3cf36aedc1a52ba0
shibafu <shibafu528@gmail.com>
2020-05-20 21:07:11 +0900

    use custom control style

--- resources/views/setting/export.blade.php
+++ resources/views/setting/export.blade.php
@@ -9,11 +9,15 @@
     <form class="mt-4" action="{{ route('setting.export.csv') }}" method="get">
         {{ csrf_field() }}
         <div class="mb-2"><strong>文字コード</strong></div>
-        <div class="form-check">
-            <label><input class="form-check-input" type="radio" name="charset" value="utf8" checked>UTF-8</label>
-        </div>
-        <div class="form-check">
-            <label><input class="form-check-input" type="radio" name="charset" value="sjis">Shift_JIS</label>
+        <div class="form-group">
+            <div class="custom-control custom-radio custom-control-inline">
+                <input id="charsetUTF8" class="custom-control-input" type="radio" name="charset" value="utf8" checked>
+                <label for="charsetUTF8" class="custom-control-label">UTF-8</label>
+            </div>
+            <div class="custom-control custom-radio custom-control-inline ml-3">
+                <input id="charsetSJIS" class="custom-control-input" type="radio" name="charset" value="sjis">
+                <label for="charsetSJIS" class="custom-control-label">Shift_JIS</label>
+            </div>
         </div>
         <button type="submit" class="btn btn-primary mt-3">ダウンロード</button>
     </form>
