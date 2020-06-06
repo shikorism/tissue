c8c278653af800b6dcb0bfbf76eba1ea299a012d
hina <hina@hinaloe.net>
2020-06-06 17:37:01 +0900

    eslintの仮構成

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/vue",
+    ],
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        ecmaVersion: 11,
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+    },
+    plugins: ["prettier", "vue", "@typescript-eslint"],
+    rules: {},
+};
