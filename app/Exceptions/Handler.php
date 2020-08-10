da7be61698c7c9f82e06a86c816f84b3c4668293
shibafu <shibafu528@gmail.com>
2020-08-10 13:40:40 +0900

    Don't report circuit break exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -20,6 +20,7 @@ class Handler extends ExceptionHandler
         \Illuminate\Database\Eloquent\ModelNotFoundException::class,
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
+        \App\MetadataResolver\ResolverCircuitBreakException::class,
     ];
 
     /**
