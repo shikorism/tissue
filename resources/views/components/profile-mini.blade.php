61b83c18a780b0f815ea6d6c9ae8c662e30d57a6
shibafu <shibafu528@gmail.com>
2020-10-24 18:30:13 +0900

    ユーザー名のスタイル調整

--- resources/views/components/profile-mini.blade.php
+++ resources/views/components/profile-mini.blade.php
@@ -1,14 +1,14 @@
 <div class="d-flex flex-row align-items-end {{ $class ?? '' }}">
     <img src="{{ $user->getProfileImageUrl(48) }}" srcset="{{ Formatter::profileImageSrcSet($user, 48) }}" class="rounded mr-2">
     <div class="d-flex flex-column overflow-hidden">
-        <h5 class="card-title text-truncate">
+        <div class="tis-profile-mini-display-name text-truncate">
             <a class="text-dark" href="{{ route('user.profile', ['name' => $user->name]) }}">{{ $user->display_name }}</a>
-        </h5>
-        <h6 class="card-subtitle">
+        </div>
+        <div class="tis-profile-mini-name">
             <a class="text-muted" href="{{ route('user.profile', ['name' => $user->name]) }}">&commat;{{ $user->name }}</a>
             @if ($user->is_protected)
                 <span class="oi oi-lock-locked text-muted"></span>
             @endif
-        </h6>
+        </div>
     </div>
 </div>
