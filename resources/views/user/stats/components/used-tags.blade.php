24170a1d253a4f6fba7157763f856d625fb6d0a2
shibafu <shibafu528@gmail.com>
2020-12-12 14:22:55 +0900

    2カラム化

--- resources/views/user/stats/components/used-tags.blade.php
+++ resources/views/user/stats/components/used-tags.blade.php
@@ -1,35 +1,40 @@
-<h5 class="my-4">最も使用したタグ</h5>
-@if ($tags->isEmpty())
-    <div class="alert alert-secondary">
-        データがありません
+<div class="row">
+    <div class="col-md-6">
+        <h5 class="mb-4">最も使用したタグ</h5>
+        @if ($tags->isEmpty())
+            <div class="alert alert-secondary">
+                データがありません
+            </div>
+        @else
+            <table class="table table-striped border">
+                <tbody>
+                @foreach ($tags as $tag)
+                    <tr>
+                        <td><a class="text-reset" href="{{ route('search', ['q' => $tag->name]) }}"><span class="oi oi-tag text-secondary mr-2"></span>{{ $tag->name }}</a></td>
+                        <td class="text-right">{{ number_format($tag->count) }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
     </div>
-@else
-    <table class="table table-striped border">
-        <tbody>
-        @foreach ($tags as $tag)
-            <tr>
-                <td><a class="text-reset" href="{{ route('search', ['q' => $tag->name]) }}"><span class="oi oi-tag text-secondary mr-2"></span>{{ $tag->name }}</a></td>
-                <td class="text-right">{{ number_format($tag->count) }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-@endif
-<hr class="my-4">
-<h5 class="my-4">最も使用したタグ (オカズのタグを含む)</h5>
-@if ($tagsIncludesMetadata->isEmpty())
-    <div class="alert alert-secondary">
-        データがありません
+    <div class="col-md-6">
+        <h5 class="mb-4">オカズを含む最も使用したタグ</h5>
+        @if ($tagsIncludesMetadata->isEmpty())
+            <div class="alert alert-secondary">
+                データがありません
+            </div>
+        @else
+            <table class="table table-striped border">
+                <tbody>
+                @foreach ($tagsIncludesMetadata as $tag)
+                    <tr>
+                        <td><a class="text-reset" href="{{ route('search', ['q' => $tag->name]) }}"><span class="oi oi-tag text-secondary mr-2"></span>{{ $tag->name }}</a></td>
+                        <td class="text-right">{{ number_format($tag->count) }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
     </div>
-@else
-    <table class="table table-striped border">
-        <tbody>
-        @foreach ($tagsIncludesMetadata as $tag)
-            <tr>
-                <td><a class="text-reset" href="{{ route('search', ['q' => $tag->name]) }}"><span class="oi oi-tag text-secondary mr-2"></span>{{ $tag->name }}</a></td>
-                <td class="text-right">{{ number_format($tag->count) }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-@endif
+</div>
