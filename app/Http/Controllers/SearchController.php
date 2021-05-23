479d952fd803797680a29aa18882c361dec5b924
shibafu <shibafu528@gmail.com>
2021-05-23 16:53:31 +0900

    exprの解釈を書いた

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Ejaculation;
+use App\Parser\SearchQuery\InvalidExpressionException;
+use App\Parser\SearchQueryParser;
 use App\Tag;
 use App\Utilities\Formatter;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,9 +29,6 @@ class SearchController extends Controller
 
         $q = $this->normalizeQuery($inputs['q']);
         $results = Ejaculation::query()
-            ->whereHas('tags', function ($query) use ($q) {
-                $query->where('normalized_name', 'like', "%{$q}%");
-            })
             ->whereHas('user', function ($query) {
                 $query->where('is_protected', false);
                 if (Auth::check()) {
@@ -39,9 +39,60 @@ class SearchController extends Controller
             ->orderBy('ejaculated_date', 'desc')
             ->with(['user', 'tags'])
             ->withLikes()
-            ->withMutedStatus()
-            ->paginate(20)
-            ->appends($inputs);
+            ->withMutedStatus();
+
+        try {
+            $parser = (new SearchQueryParser())->parse($q);
+            foreach ($parser->getExpressions() as $expression) {
+                switch ($expression->target) {
+                    case 'date': {
+                        $op = $expression->negative ? '<>' : '=';
+                        $results = $results->whereDate('ejaculated_date', $op, $expression->keyword);
+                        break;
+                    }
+                    case 'since':
+                        $results = $results->whereDate('ejaculated_date', '>=', $expression->keyword);
+                        break;
+                    case 'until':
+                        $results = $results->whereDate('ejaculated_date', '<=', $expression->keyword);
+                        break;
+                    case 'link':
+                    case 'url': {
+                        $op = $expression->negative ? 'not like' : 'like';
+                        $results = $results->where('link', $op, "%{$expression->keyword}%");
+                        break;
+                    }
+                    case 'note': {
+                        $op = $expression->negative ? 'not like' : 'like';
+                        $results = $results->where('note', $op, "%{$expression->keyword}%");
+                        break;
+                    }
+                    case 'tag':
+                        $results = $results->whereHas('tags', function ($query) use ($expression) {
+                            $op = $expression->negative ? 'not like' : 'like';
+                            $query->where('normalized_name', $op, "%{$expression->keyword}%");
+                        });
+                        break;
+                    case 'user':
+                        $results = $results->whereHas('user', function ($query) use ($expression) {
+                            $op = $expression->negative ? '<>' : '=';
+                            $query->where('name', $op, $expression->keyword);
+                        });
+                        break;
+                    case 'is':
+                        switch ($expression->keyword) {
+                            case 'sensitive':
+                                $results = $results->where('is_too_sensitive', !$expression->negative);
+                                break;
+                        }
+                        break;
+                }
+            }
+        } catch (InvalidExpressionException $e) {
+            $results = collect();
+        }
+
+        $results = $results->paginate(20)->appends($inputs);
 
         return view('search.index')->with(compact('inputs', 'results'));
     }
