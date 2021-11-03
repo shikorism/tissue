78fe96ae2982e67738d4e4f4af0d915cfd66195f
shibafu <shibafu528@gmail.com>
2021-11-04 00:05:35 +0900

    チェックイン削除をAjaxでやるようにした

--- resources/views/components/delete-checkin-modal.blade.php
+++ resources/views/components/delete-checkin-modal.blade.php
@@ -4,9 +4,7 @@
     @endslot
     <span class="date-label"></span> のチェックインを削除してもよろしいですか？
     @slot('footer')
-        <form action="{{ route('checkin.destroy', ['id' => '@']) }}" method="post">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
+        <form>
             <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
             <button type="submit" class="btn btn-danger">削除</button>
         </form>
