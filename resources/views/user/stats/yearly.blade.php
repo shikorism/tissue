ee26c88c650647a09f9bce03f0b44c4404c50690
shibafu <shibafu528@gmail.com>
2021-12-30 17:25:21 +0900

    今年よく使ったオカズのランキングを年間統計ページに追加

--- resources/views/user/stats/yearly.blade.php
+++ resources/views/user/stats/yearly.blade.php
@@ -20,6 +20,27 @@
     <canvas id="dow-graph" class="w-100"></canvas>
     <hr class="my-4">
     @include('user.stats.components.used-tags')
+    <hr class="my-4">
+    <h5 class="my-4">最も使ったオカズ</h5>
+    <p class="text-secondary">2回以上使用したオカズのみ集計しています。</p>
+    <ul class="list-group">
+        @forelse ($mostFrequentlyUsedRanking as $index => $item)
+            <li class="list-group-item border-bottom-only pt-3 pb-3 text-break">
+                <p class="mb-1"><span class="mr-3 tis-rank-badge">{{ $index + 1 }}</span><span class="mr-2" style="font-size: 1.75rem">{{ $item->count }}</span>回</p>
+                <div class="row mx-0">
+                    @component('components.link-card', ['link' => $item->normalized_link, 'is_too_sensitive' => false])
+                    @endcomponent
+                    <p class="d-flex align-items-baseline mb-2 col-12 px-0">
+                        <span class="oi oi-link-intact mr-1"></span><a class="overflow-hidden" href="{{ $item->normalized_link }}" target="_blank" rel="noopener">{{ $item->normalized_link }}</a>
+                    </p>
+                </div>
+            </li>
+        @empty
+            <li class="list-group-item border-bottom-only">
+                <p>この期間のチェックインが無いか、2回以上使用したオカズがありません。</p>
+            </li>
+        @endforelse
+    </ul>
 @endsection
 
 @push('script')
