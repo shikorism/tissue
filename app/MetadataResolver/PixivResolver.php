56f7f9f508217534b79ef7a523f8817f521bf846
Irie Aoi <eai@mizle.net>
2023-03-15 04:22:14 +0900

    既存の仕様に準拠してsquareではなくmasterを使用する

--- app/MetadataResolver/PixivResolver.php
+++ app/MetadataResolver/PixivResolver.php
@@ -64,7 +64,7 @@ class PixivResolver implements Resolver
         $metadata->title = $json['body']['illustTitle'] ?? '';
         $metadata->description = '投稿者: ' . $json['body']['userName'] . PHP_EOL . strip_tags(str_replace('<br />', PHP_EOL, $json['body']['illustComment'] ?? ''));
         $metadata->image = $this->proxize(
-            str_replace("/c/250x250_80_a2", "", $json['body']['userIllusts'][$illustId]['url'])
+            str_replace(['/c/250x250_80_a2', '_square1200'], ['', '_master1200'], $json['body']['userIllusts'][$illustId]['url'])
                 ?? ''
         );
 
