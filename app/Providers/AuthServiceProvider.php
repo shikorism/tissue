160249d6501cf5c2bfa49a4cffde8d664602b052
shibafu <shibafu528@gmail.com>
2024-12-01 10:58:31 +0900

    [laravel 10] import laravel/laravel:10.x defaults

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * The policy mappings for the application.
      *
-     * @var array
+     * @var array<class-string, class-string>
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
@@ -22,10 +22,8 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         Passport::hashClientSecrets();
         Passport::personalAccessTokensExpireIn(now()->addYears(10));
