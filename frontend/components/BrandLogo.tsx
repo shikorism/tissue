ba036308b6d242f3841bec92f7aa5c56bc2ac5a2
shibafu <shibafu528@gmail.com>
2025-03-24 21:44:31 +0900

    /app

---
+++ frontend/components/BrandLogo.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export const BrandLogo: React.FC = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        strokeLinejoin="round"
+        viewBox="0 0 64 64"
+        height={48}
+    >
+        <circle cx="8.529" cy="288.381" r="7.651" fill="#e040fb" transform="translate(0 -1058.52)scale(3.77953)" />
+        <path
+            d="M3.801 8.32h9.509v3.769H3.801z"
+            fill="none"
+            stroke="#fff"
+            strokeWidth=".79px"
+            transform="scale(3.77953)"
+        />
+        <path
+            d="m5.637 8.306-.574-3.795c.272 1.536 1.788 2.27 3.659 1.175.296.621 1.361 1.863 3.35.542L11.6 8.306z"
+            fill="#fff"
+            stroke="#fff"
+            strokeWidth=".78px"
+            transform="scale(3.77953)"
+        />
+        <path
+            d="M3.75 10.874h9.84"
+            fill="none"
+            stroke="#fff"
+            strokeWidth=".53px"
+            strokeLinejoin="miter"
+            transform="scale(3.77953)"
+        />
+    </svg>
+);
