e4844393bd879ff36cfeb370df0b1b6621ba083e
shibafu <shibafu528@gmail.com>
2025-03-29 16:43:49 +0900

    パスから /app を削除

--- frontend/components/GlobalHeader.tsx
+++ frontend/components/GlobalHeader.tsx
@@ -8,22 +8,22 @@ export const GlobalHeader: React.FC = () => {
 
     return (
         <nav className="px-4 py-2 bg-gray-back flex justify-between items-center">
-            <Link to="/app" className="flex items-center gap-3">
+            <Link to="/" className="flex items-center gap-3">
                 <BrandLogo />
                 <span className="text-xl opacity-90">Tissue</span>
             </Link>
             {me ? (
                 <div className="flex gap-2">
-                    <Link to="/app/checkin" className="p-2 opacity-70 hover:opacity-90 focus:opacity-90">
+                    <Link to="/checkin" className="p-2 opacity-70 hover:opacity-90 focus:opacity-90">
                         チェックイン
                     </Link>
                 </div>
             ) : UnauthorizedError.castFrom(error) ? (
                 <div className="flex gap-2">
-                    <Link to="/app/register" className="p-2 opacity-70 hover:opacity-90 focus:opacity-90">
+                    <Link to="/register" className="p-2 opacity-70 hover:opacity-90 focus:opacity-90">
                         会員登録
                     </Link>
-                    <Link to="/app/login" className="p-2 opacity-70 hover:opacity-90 focus:opacity-90">
+                    <Link to="/login" className="p-2 opacity-70 hover:opacity-90 focus:opacity-90">
                         ログイン
                     </Link>
                 </div>
