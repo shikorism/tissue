ff45e41abe0bfae7791a61248020a1a2a5a55e00
shibafu <shibafu528@gmail.com>
2025-04-23 01:33:35 +0900

    account on global nav

--- frontend/components/GlobalNavigation.tsx
+++ frontend/components/GlobalNavigation.tsx
@@ -7,55 +7,69 @@ export const GlobalNavigation: React.FC = () => {
     const { data: me } = useGetMe();
 
     return (
-        <nav className="hidden md:block fixed left-0 top-0 bottom-0 w-(--global-nav-width) p-4 bg-gray-back">
+        <nav className="hidden md:flex flex-col fixed left-0 top-0 bottom-0 w-(--global-nav-width) p-4 bg-gray-back">
             <Link to="/" className="flex items-center gap-3 mb-2">
                 <BrandLogo />
                 <span className="text-xl opacity-90">Tissue</span>
             </Link>
-            <ul>
-                <NavItem to="/">
-                    <i className="ti ti-home text-2xl text-neutral-600"></i>ホーム
-                </NavItem>
-                <NavItem to="/search">
-                    <i className="ti ti-search text-2xl text-neutral-600"></i>検索
-                </NavItem>
-                <NavItem to="/timeline/public">
-                    <i className="ti ti-layout-grid text-2xl text-neutral-600"></i>お惣菜
-                </NavItem>
-                <NavItem to="/tag">
-                    <i className="ti ti-tags text-2xl text-neutral-600"></i>タグ一覧
-                </NavItem>
-            </ul>
+            <div className="flex-1">
+                <ul>
+                    <NavItem to="/">
+                        <i className="ti ti-home text-2xl text-neutral-600"></i>ホーム
+                    </NavItem>
+                    <NavItem to="/search">
+                        <i className="ti ti-search text-2xl text-neutral-600"></i>検索
+                    </NavItem>
+                    <NavItem to="/timeline/public">
+                        <i className="ti ti-layout-grid text-2xl text-neutral-600"></i>お惣菜
+                    </NavItem>
+                    <NavItem to="/tag">
+                        <i className="ti ti-tags text-2xl text-neutral-600"></i>タグ一覧
+                    </NavItem>
+                </ul>
+                {me && (
+                    <>
+                        <h2 className="mt-4 mb-2 px-2 text-sm font-bold">記録する</h2>
+                        <ul>
+                            <NavItem to="/checkin">
+                                <i className="ti ti-send text-2xl text-neutral-600"></i>チェックイン
+                            </NavItem>
+                            <NavItem to="/collect">
+                                <i className="ti ti-folder-plus text-2xl text-neutral-600"></i>コレクションに追加
+                            </NavItem>
+                        </ul>
+                        <h2 className="mt-4 mb-2 px-2 text-sm font-bold">マイデータ</h2>
+                        <ul>
+                            <NavItem to={`/user/${me.name}`}>
+                                <i className="ti ti-list text-2xl text-neutral-600"></i>タイムライン
+                            </NavItem>
+                            <NavItem to={`/user/${me.name}/stats`}>
+                                <i className="ti ti-timeline text-2xl text-neutral-600"></i>グラフ
+                            </NavItem>
+                            <NavItem to={`/user/${me.name}/okazu`}>
+                                <i className="ti ti-books text-2xl text-neutral-600"></i>オカズ
+                            </NavItem>
+                            <NavItem to={`/user/${me.name}/likes`}>
+                                <i className="ti ti-heart text-2xl text-neutral-600"></i>いいね履歴
+                            </NavItem>
+                            <NavItem to={`/user/${me.name}/collections`}>
+                                <i className="ti ti-folder text-2xl text-neutral-600"></i>コレクション
+                            </NavItem>
+                        </ul>
+                    </>
+                )}
+            </div>
             {me && (
-                <>
-                    <h2 className="mt-4 mb-2 px-2 text-sm font-bold">記録する</h2>
-                    <ul>
-                        <NavItem to="/checkin">
-                            <i className="ti ti-send text-2xl text-neutral-600"></i>チェックイン
-                        </NavItem>
-                        <NavItem to="/collect">
-                            <i className="ti ti-folder-plus text-2xl text-neutral-600"></i>コレクションに追加
-                        </NavItem>
-                    </ul>
-                    <h2 className="mt-4 mb-2 px-2 text-sm font-bold">マイデータ</h2>
-                    <ul>
-                        <NavItem to={`/user/${me.name}`}>
-                            <i className="ti ti-list text-2xl text-neutral-600"></i>タイムライン
-                        </NavItem>
-                        <NavItem to={`/user/${me.name}/stats`}>
-                            <i className="ti ti-timeline text-2xl text-neutral-600"></i>グラフ
-                        </NavItem>
-                        <NavItem to={`/user/${me.name}/okazu`}>
-                            <i className="ti ti-books text-2xl text-neutral-600"></i>オカズ
-                        </NavItem>
-                        <NavItem to={`/user/${me.name}/likes`}>
-                            <i className="ti ti-heart text-2xl text-neutral-600"></i>いいね履歴
-                        </NavItem>
-                        <NavItem to={`/user/${me.name}/collections`}>
-                            <i className="ti ti-folder text-2xl text-neutral-600"></i>コレクション
-                        </NavItem>
-                    </ul>
-                </>
+                <div className="mt-4 p-2 flex items-center gap-1 rounded-sm hover:bg-neutral-200 cursor-pointer">
+                    <img
+                        className="rounded inline-block align-bottom mr-1"
+                        src={me.profile_image_url}
+                        alt={`${me.display_name}'s Avatar`}
+                        width={40}
+                        height={40}
+                    />
+                    <span className="text-sm overflow-ellipsis overflow-hidden text-nowrap">{me.display_name}</span>
+                </div>
             )}
         </nav>
     );
