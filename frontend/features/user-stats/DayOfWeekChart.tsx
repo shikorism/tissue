06233606ea81828ce88718c71743a1fc6a0cac93
shibafu <shibafu528@gmail.com>
2025-05-26 23:24:42 +0900

    UserStatsYearly

--- frontend/features/user-stats/DayOfWeekChart.tsx
+++ frontend/features/user-stats/DayOfWeekChart.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import { barChartDatasetDefaults, barChartOptions } from './chart';
+import { barChartDatasetDefaults, barChartDatasetDefaults2, barChartOptions } from './chart';
+
+type Stats = { date: string; count: number }[];
 
 interface Props {
-    dailyStats: { date: string; count: number }[];
+    dailyStats: Stats;
+    compareDailyStats?: Stats;
 }
 
-export const DayOfWeekChart: React.FC<Props> = ({ dailyStats }) => {
-    const dowSum: number[] = new Array(7).fill(0);
-    for (const daily of dailyStats) {
-        const dow = new Date(`${daily.date}T00:00:00+09:00`).getDay();
-        dowSum[dow] += daily.count;
+export const DayOfWeekChart: React.FC<Props> = ({ dailyStats, compareDailyStats }) => {
+    const datasets = [{ ...barChartDatasetDefaults, data: summarize(dailyStats) }];
+    if (compareDailyStats) {
+        datasets.push({ ...barChartDatasetDefaults2, data: summarize(compareDailyStats) });
     }
 
     return (
         <Bar
             data={{
                 labels: ['日', '月', '火', '水', '木', '金', '土'],
-                datasets: [{ ...barChartDatasetDefaults, data: dowSum }],
+                datasets,
             }}
             options={barChartOptions}
         />
     );
+};
+
+const summarize = (dailyStats: Stats) => {
+    const dowSum: number[] = new Array(7).fill(0);
+    for (const daily of dailyStats) {
+        const dow = new Date(`${daily.date}T00:00:00+09:00`).getDay();
+        dowSum[dow] += daily.count;
+    }
+    return dowSum;
 };
