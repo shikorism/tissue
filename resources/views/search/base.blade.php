38a229f4a5d5c7492443d225b2a3f31c711049a4
shibafu <shibafu528@gmail.com>
2026-01-21 21:36:38 +0900

    protected_only_modeではタグ検索機能を停止する (#1645)

--- resources/views/search/base.blade.php
+++ resources/views/search/base.blade.php
@@ -12,9 +12,11 @@
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() === 'search.collection' ? 'active' : '' }}" href="{{ route('search.collection', $inputs) }}">コレクション</a>
             </li>
+            @unless (config('app.protected_only_mode'))
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() === 'search.related-tag' ? 'active' : '' }}" href="{{ route('search.related-tag', $inputs) }}">関連するタグ</a>
             </li>
+            @endunless
         </ul>
         <div class="tab-content">
             <p class="my-3 text-secondary">{{ $results->total() }} 件見つかりました</p>
