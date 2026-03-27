49983a42dd64a4b0b920fce365ea4a015ee781f1
shibafu <shibafu528@gmail.com>
2026-03-27 12:31:20 +0900

    プロフィールページにあったアクティビティ情報をホームに移動

--- frontend/pages/Home.loader.ts
+++ frontend/pages/Home.loader.ts
@@ -1,9 +1,38 @@
 import { QueryClient } from '@tanstack/react-query';
-import { getInformationLatestQuery, getMeQuery } from '../api/query';
+import {
+    getInformationLatestQuery,
+    getMeQuery,
+    getUserCheckinsQuery,
+    getUserStatsCheckinDailyQuery,
+    getUserStatsTagsQuery,
+} from '../api/query';
+import { endOfMonth, formatDate, startOfMonth, subMonths } from 'date-fns';
+
+export interface LoaderData {
+    statsCheckinDailyQuery: {
+        since: string;
+        until: string;
+    };
+}
 
 export const loader = (queryClient: QueryClient) => async () => {
-    await Promise.all([
-        queryClient.prefetchQuery(getMeQuery()), // ステータス欄の情報を最新にするため、常に再読み込み
+    const [me] = await Promise.all([
+        queryClient.fetchQuery(getMeQuery()), // ステータス欄の情報を最新にするため、常に再読み込み
         queryClient.ensureQueryData(getInformationLatestQuery()),
     ]);
+    if (!me) {
+        return;
+    }
+
+    const now = new Date();
+    const statsCheckinDailyQuery = {
+        since: formatDate(subMonths(startOfMonth(now), 11), 'yyyy-MM-dd'),
+        until: formatDate(endOfMonth(now), 'yyyy-MM-dd'),
+    };
+    await Promise.all([
+        queryClient.ensureQueryData(getUserStatsTagsQuery(me.name)),
+        queryClient.ensureQueryData(getUserStatsCheckinDailyQuery(me.name, statsCheckinDailyQuery)),
+        queryClient.ensureQueryData(getUserCheckinsQuery(me.name)),
+    ]);
+    return { statsCheckinDailyQuery } satisfies LoaderData;
 };
--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router';
 import { subSeconds, format } from 'date-fns';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { getInformationLatestQuery, getMeQuery } from '../api/query';
+import {
+    getInformationLatestQuery,
+    getMeQuery,
+    getUserCheckinsQuery,
+    getUserStatsCheckinDailyQuery,
+    getUserStatsTagsQuery,
+} from '../api/query';
 import { formatOrDefault, formatNumber, formatInterval } from '../lib/formatter';
 import { Container } from '../components/Container';
 import { categories } from '../features/info/categories';
 import { cn } from '../lib/cn';
+import { LoaderData } from './Home.loader';
+import type { components } from '../api/schema';
+import { CheckinHeatmap } from '../features/user-stats/CheckinHeatmap';
+import { Checkin } from '../features/checkins/Checkin';
 
 export const Home: React.FC = () => {
     const { data: me } = useSuspenseQuery(getMeQuery());
@@ -47,59 +58,155 @@ export const Home: React.FC = () => {
                 </div>
             )}
 
-            <Container>
-                <div className="p-3 flex flex-col md:flex-row border-1 border-gray-border rounded">
-                    <div className="flex-1 text-center md:text-start">
-                        <h1 className="text-lg font-bold">現在のセッション</h1>
-                        <p className="my-2 text-xl">
-                            {formatOrDefault(me?.checkin_summary?.current_session_elapsed, formatInterval)}
-                        </p>
-                        <p className="text-sm">
-                            {me
-                                ? me.checkin_summary
-                                    ? `${format(
-                                          subSeconds(Date.now(), me.checkin_summary.current_session_elapsed),
-                                          'yyyy/MM/dd HH:mm',
-                                      )} にリセット`
-                                    : '計測がまだ始まっていません'
-                                : '\u{2015}'}
-                        </p>
-                    </div>
-                    <table className="flex-1 text-sm mt-2 md:mt-0">
-                        <tbody>
-                            <tr>
-                                <th className="pr-2 py-1 text-right w-2/5 after:content-[':']">通算回数</th>
-                                <td>{formatOrDefault(me?.checkin_summary?.total_checkins, formatNumber)}回</td>
-                            </tr>
-                            <tr>
-                                <th className="pr-2 py-1 text-right after:content-[':']">平均記録</th>
-                                <td>{formatOrDefault(me?.checkin_summary?.average_interval, formatInterval)}</td>
-                            </tr>
-                            <tr>
-                                <th className="pr-2 py-1 text-right after:content-[':']">中央値</th>
-                                <td>{formatOrDefault(me?.checkin_summary?.median_interval, formatInterval)}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <table className="flex-1 text-sm">
-                        <tbody>
-                            <tr>
-                                <th className="pr-2 py-1 text-right w-2/5 after:content-[':']">最長記録</th>
-                                <td>{formatOrDefault(me?.checkin_summary?.longest_interval, formatInterval)}</td>
-                            </tr>
-                            <tr>
-                                <th className="pr-2 py-1 text-right after:content-[':']">最短記録</th>
-                                <td>{formatOrDefault(me?.checkin_summary?.shortest_interval, formatInterval)}</td>
-                            </tr>
-                            <tr>
-                                <th className="pr-2 py-1 text-right after:content-[':']">合計時間</th>
-                                <td>{formatOrDefault(me?.checkin_summary?.total_times, formatInterval)}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </Container>
+            {me && (
+                <Container className="flex flex-col gap-4">
+                    <h2 className="text-xl font-bold">アクティビティ</h2>
+                    <CurrentSession user={me} />
+                    <RecentActivity user={me} />
+                    <MostlyUsedTags user={me} />
+                    <RecentCheckin user={me} />
+                </Container>
+            )}
         </>
+    );
+};
+
+interface CurrentSessionProps {
+    user: components['schemas']['User'];
+}
+
+const CurrentSession: React.FC<CurrentSessionProps> = ({ user }) => {
+    return (
+        <div className="p-3 flex flex-col md:flex-row border-1 border-gray-border rounded">
+            <div className="flex-1 text-start">
+                <h3 className="text-lg font-bold">現在のセッション</h3>
+                <p className="my-2 text-xl">
+                    {formatOrDefault(user.checkin_summary?.current_session_elapsed, formatInterval)}
+                </p>
+                <p className="text-sm">
+                    {user.checkin_summary
+                        ? `${format(
+                              subSeconds(Date.now(), user.checkin_summary.current_session_elapsed),
+                              'yyyy/MM/dd HH:mm',
+                          )} にリセット`
+                        : '計測がまだ始まっていません'}
+                </p>
+            </div>
+            <table className="flex-1 text-sm mt-4 md:mt-0 self-start md:self-auto">
+                <tbody>
+                    <tr>
+                        <th className="pr-2 py-1 text-right md:w-2/5 after:content-[':']">通算回数</th>
+                        <td>{formatOrDefault(user.checkin_summary?.total_checkins, formatNumber)}回</td>
+                    </tr>
+                    <tr>
+                        <th className="pr-2 py-1 text-right after:content-[':']">平均記録</th>
+                        <td>{formatOrDefault(user.checkin_summary?.average_interval, formatInterval)}</td>
+                    </tr>
+                    <tr>
+                        <th className="pr-2 py-1 text-right after:content-[':']">中央値</th>
+                        <td>{formatOrDefault(user.checkin_summary?.median_interval, formatInterval)}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table className="flex-1 text-sm self-start md:self-auto">
+                <tbody>
+                    <tr>
+                        <th className="pr-2 py-1 text-right md:w-2/5 after:content-[':']">最長記録</th>
+                        <td>{formatOrDefault(user.checkin_summary?.longest_interval, formatInterval)}</td>
+                    </tr>
+                    <tr>
+                        <th className="pr-2 py-1 text-right after:content-[':']">最短記録</th>
+                        <td>{formatOrDefault(user.checkin_summary?.shortest_interval, formatInterval)}</td>
+                    </tr>
+                    <tr>
+                        <th className="pr-2 py-1 text-right after:content-[':']">合計時間</th>
+                        <td>{formatOrDefault(user.checkin_summary?.total_times, formatInterval)}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+interface RecentActivityProps {
+    user: components['schemas']['User'];
+}
+
+const RecentActivity: React.FC<RecentActivityProps> = ({ user }) => {
+    const { statsCheckinDailyQuery } = useLoaderData<LoaderData>();
+    const { data: checkinStats } = useSuspenseQuery(getUserStatsCheckinDailyQuery(user.name, statsCheckinDailyQuery));
+
+    return (
+        <div className="p-3 border-1 border-gray-border rounded">
+            <div className="mb-2 flex items-baseline gap-4">
+                <h3 className="text-lg font-bold">最近の活動</h3>
+                <Link to={`/user/${user.name}/stats`} className="text-primary hover:brightness-80 hover:underline">
+                    グラフを見る &raquo;
+                </Link>
+            </div>
+            <div className="mt-2 overflow-x-auto">
+                <CheckinHeatmap startDate={statsCheckinDailyQuery.since} data={checkinStats} />
+            </div>
+        </div>
+    );
+};
+
+interface MostlyUsedTagsProps {
+    user: components['schemas']['User'];
+}
+
+const MostlyUsedTags: React.FC<MostlyUsedTagsProps> = ({ user }) => {
+    const { data: tags } = useSuspenseQuery(getUserStatsTagsQuery(user.name));
+
+    return (
+        <div className="p-3 border-1 border-gray-border rounded">
+            <h3 className="text-lg font-bold">よく使っているタグ</h3>
+            <ul className="mt-2 flex flex-wrap gap-3">
+                {tags.map((tag) => (
+                    <li key={tag.name}>
+                        <Link
+                            to={{ pathname: `/search`, search: `?q=${tag.name}` }}
+                            className="group inline-block max-w-full break-all whitespace-normal"
+                        >
+                            <i className="ti ti-tag mr-1" />
+                            <span className="text-primary group-hover:brightness-80 group-hover:underline">
+                                {tag.name}
+                            </span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+interface RecentCheckinProps {
+    user: components['schemas']['User'];
+}
+
+const RecentCheckin: React.FC<RecentCheckinProps> = ({ user }) => {
+    const { data: checkins, refetch } = useSuspenseQuery(getUserCheckinsQuery(user.name));
+    if (checkins.data.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className="p-3 border-1 border-gray-border rounded">
+            <div className="flex items-baseline gap-4">
+                <h3 className="text-lg font-bold">最新のチェックイン</h3>
+                <Link to={`/user/${user.name}/checkins`} className="text-primary hover:brightness-80 hover:underline">
+                    もっと見る &raquo;
+                </Link>
+            </div>
+            <Checkin
+                className="pt-2 pb-0"
+                key={checkins.data[0].id}
+                checkin={checkins.data[0]}
+                intervalStyle="full"
+                showActions
+                onDelete={() => refetch()}
+            />
+        </div>
     );
 };
 
