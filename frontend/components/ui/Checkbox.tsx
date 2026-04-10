bd539d2dee1eb75240d0ea11f38d9dee6c12c184
shibafu <shibafu528@gmail.com>
2026-04-10 20:05:47 +0900

    fix: Checkbox 別に再帰する必要はないので変えた

--- frontend/components/ui/Checkbox.tsx
+++ frontend/components/ui/Checkbox.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { cn } from '../../lib/cn';
 
-export const Checkbox: React.FC<React.ComponentProps<'input'>> = ({ className, children, ...props }) =>
-    children ? (
+export const Checkbox: React.FC<React.ComponentProps<'input'>> = ({ className, children, ...props }) => {
+    const checkbox = <input type="checkbox" className={cn('accent-primary', className)} {...props} />;
+    return children ? (
         <label>
-            <Checkbox {...props} />
+            {checkbox}
             <span className="ml-2">{children}</span>
         </label>
     ) : (
-        <input type="checkbox" className={cn('accent-primary', className)} {...props} />
+        checkbox
     );
+};
